cf80a56843baecd0f41649f132e493fb46417195
6119 <6119@6119-23>
2017-03-23 11:35:11 +0900

    끝

--- Data Structure_2017_03_22_(2)/Data Structure_2017_03_22_(2)/Source.c
+++ Data Structure_2017_03_22_(2)/Data Structure_2017_03_22_(2)/Source.c
@@ -6,15 +6,26 @@ int main()
 	int a[ROW][COL] = { { 1,0,0 },{ 1,0,0 },{ 1,0,0 } };
 	int b[ROW][COL] = { { 1,1,1 },{ 0,0,0 },{ 0,0,0 } };
 	int c[ROW][COL] = { { 0,0,0 },{ 0,0,0 },{ 0,0,0 } };
+
+	/*matrix_print(a);
+	printf("\n");
+	matrix_print(b);
+	printf("\n");
+	matrix_print(c);
+	printf("\n");
+*/
 	matrix_print(a);
+	printf("\n");
 	matrix_print(b);
 	matrix_add(a, b, c);
+	
 	matrix_print(c);
 	matrix_init(c);
 	matrix_mult(a, b, c);
 	matrix_print(c);
+	
 	matrix_init(c);
-	matrix_trans(a, c);
+	matrix_transpose(a, c);
 	matrix_print(c);
 
 	return 0;
@@ -29,6 +40,7 @@ void matrix_init(int a[ROW][COL])
 
 void matrix_add(int a[ROW][COL], int b[ROW][COL], int c[ROW][COL])
 {
+	printf("\na + b = \n");
 	for(int i = 0; i<ROW;i++)
 		for (int j = 0; j < COL; j++)
 		{
@@ -38,11 +50,41 @@ void matrix_add(int a[ROW][COL], int b[ROW][COL], int c[ROW][COL])
 
 void matrix_mult(int a[ROW][COL], int b[ROW][COL], int c[ROW][COL])
 {
+	printf("\na * b = \n");
+	for (int i = 0; i < ROW; i++)		//i x k
+	{
+		for (int j = 0; j < COL; j++)	// k x j
+		{
+			for (int k = 0; k < COL; k++)
+			{
+				c[i][j] += a[i][k] * b[k][j];
+			}
+		}
+	}
+}
+
+
+void matrix_transpose(int a[ROW][COL],int c[ROW][COL])
+{
+	printf("\na transpose = \n");
+
+	for (int i = 0; i < ROW; i++)
+		for (int j = 0; j < COL; j++)
+			c[i][j] = a[j][i];
+
+}
+
+
+
+void matrix_print(int a[ROW][COL])
+{
 	for (int i = 0; i < ROW; i++)
 	{
 		for (int j = 0; j < COL; j++)
 		{
-
+			printf("%d ", a[i][j]);
 		}
+		printf("\n");
 	}
 }
+
