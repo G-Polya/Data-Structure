49e234b08eea965c9975e178950fc4a7fafb8b73
6119 <6119@6119-15>
2017-03-30 10:39:50 +0900

    Queue

---
+++ Queue/QueueArray.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define Q_size 100
+#define boolean unsigned char
+#define true 1
+#define false 0
+char QueueArray[Q_size];
+
+void AddQ(char item);
+char DeleteQ();
+void Q_isFull();
+void Q_isEmpty();
+void Queue_show();
+
+int rear = -1;
+int front = -1;
