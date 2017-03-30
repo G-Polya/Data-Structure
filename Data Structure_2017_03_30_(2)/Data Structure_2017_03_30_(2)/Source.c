e1366bd10f4cde51f0ca196a9482137edde2d1a1
6119 <6119@6119-15>
2017-03-30 10:54:02 +0900

    주석입력 끝

--- Data Structure_2017_03_30_(2)/Data Structure_2017_03_30_(2)/Source.c
+++ Data Structure_2017_03_30_(2)/Data Structure_2017_03_30_(2)/Source.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include "Header.h"
+
 void main()
 {
 	char exp[100]; // postfix expression
@@ -47,7 +48,7 @@ int pop()
 		return stack[top--];
 }
 
-boolean is_number(char c)
+boolean is_number(char c)				//입력이 숫자인지 아닌지 확인
 {
 	if (('0' <= c) && (c <= '9'))
 		return true;
@@ -55,7 +56,7 @@ boolean is_number(char c)
 		return false;
 }
 
-boolean is_op(char c)
+boolean is_op(char c)					//입력이 연산자인지 확인
 {
 	if (c == '+')
 		return true;
@@ -69,21 +70,21 @@ boolean is_op(char c)
 		return false;
 }
 
-int eval_postfix(char *exp)
+int eval_postfix(char *exp)				//후위표기연산
 {
 	int i = 0;
-	char op, op2, op1;
-	char token = exp[0];
+	char op, op2, op1;					
+	char token = exp[0];				//초기입력값
 	while (token != NULL)
 	{
-		if (is_number(token))
+		if (is_number(token))			
 		{
-			op = token - '0';
-			push(op);
+			op = token - '0';			//문자(char)를 아스키코드가 아닌 정수형숫자로 바꿈
+			push(op);					//입력이 숫자면 psuh 
 		}
 		else if(is_op(token))
 		{
-			op2 = pop(); op1 = pop();
+			op2 = pop(); op1 = pop();	//입력이 연산자면 마지막 두개가지고 다음의 작업
 			switch (token)
 			{
 			case '+': push(op1 + op2); break;
@@ -92,7 +93,7 @@ int eval_postfix(char *exp)
 			case '/':push(op1 / op2); break;
 			}
 		}
-		token = exp[++i];
+		token = exp[++i];				//토큰에 다음 입력값 대입
 	}
-	return pop();
+	return pop();						//연산결과 반환
 }
