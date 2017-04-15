c70326511f6e4c69b3f64310c22e0215b763bb8e
김희수 <wlskaka4@gmail.com>
2017-04-15 21:57:08 +0900

    fd

--- Homework/problem 4/2_dimensin_figure.h
+++ Homework/problem 4/2_dimensin_figure.h
@@ -17,6 +17,7 @@ typedef struct Line
 
 Line make_line(Point _p1, Point _p2);
 
+//p1,p2은 직사각형의 대각선의 양끝점
 typedef struct Rect
 {
 	Point p1;
@@ -29,8 +30,17 @@ double line_length(Line s);
 
 double rect_area(Rect r);
 
+//중심과 반지름이 주어진 원
 typedef struct Circle
 {
-	Point p1;
-	float radius;
-};
+	Point Center;
+	Point Another;
+	double Radius;
+} Circle;
+
+Circle make_Circle_1(Point _center, double _radius);
+Circle make_Circle_2(Point _center, Point _another);
+double Circle_area_1(Circle c);
+double Circle_area_2(Circle c);
+
+
--- Homework/problem 4/problem 4.cpp
+++ Homework/problem 4/problem 4.cpp
@@ -3,24 +3,33 @@
 #include "2_dimensin_figure.h"
 #include <math.h>
 
+#define PI 3.14
 
 int main()
 {
 	Point p1, p2, p3;
 	Line s;
 	Rect r;
-	Circle c;
-	double length, area;
+	Circle c1;
+	Circle c2;
+	double length, R_area, C_area1, C_area2;
 
 	p1 = make_point(1, 1);
 	p2 = make_point(3, 3);
 	s = make_line(p1, p2);
 	r = make_rect(p1, p2);
+
+	c1 = make_Circle_1(p1, 4);
+	c2 = make_Circle_2(p2, p1);
+
 	length = line_length(s);
-	area = rect_area(r);
+	R_area = rect_area(r);
+	C_area1 = Circle_area_1(c1);
+	C_area2 = Circle_area_2(c2);
 
-	printf("line_length: %f, rect_area: %f\n", length, area);
 
+	printf("line_length: %f, rect_area: %f\n", length, R_area);
+	printf("C_area1: %f, C_area2: %f\n", C_area1, C_area2);
 }
 
 Point make_point(float _x, float _y)
@@ -59,4 +68,34 @@ double line_length(Line s)
 double rect_area(Rect r)
 {
 	return fabs(r.p1.x - r.p2.x) * fabs(r.p1.y - r.p2.y);
+}
+
+
+Circle make_Circle_1(Point _center, double _radius)
+{
+	Circle c;
+	c.Center = _center;
+	c.Radius = _radius;
+
+	return c;
+}
+
+Circle make_Circle_2(Point _center, Point _another)
+{
+	Circle c;
+	c.Center = _center;
+	c.Another = _another;
+
+	return c;
+}
+
+double Circle_area_1(Circle c)
+{
+	return c.Radius * PI;
+}
+
+double Circle_area_2(Circle c)
+{
+	c.Radius = sqrt(pow(c.Center.x - c.Another.x, 2) + pow(c.Center.y - c.Another.y, 2));
+	return c.Radius * PI;
 }
