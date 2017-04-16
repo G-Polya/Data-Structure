877ef9b1f6937c34c1f916274e3cbe5ddb39b287
김희수 <wlskaka4@gmail.com>
2017-04-16 18:02:16 +0900

    asd

--- Homework/Problem 5/Source.cpp
+++ Homework/Problem 5/Source.cpp
@@ -1,91 +1,89 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "linked_stack.h"
-
+#include <stdlib.h>
 
 int main()
 {
-	head = (stack_pointer)malloc(sizeof(stack));
-	head->item = NULL;
-	head->link = NULL;
-
 	char c, e;
+	printf("********* Command *********\n");
+	printf("+<c>: Push c, -: Pop,\n");
+	printf("S: Show, Q: quit\n");
+	printf("***************************\n\n");
 
-	while (1)
-	{
+
+	while (1) {
 		printf("\nCommand> ");
-		c = _getch();
-		_putch(c);
+		c = _getch();									//문자입력
+		_putch(c);										//입력받자마자 출력
 		c = toupper(c);
 		switch (c)
 		{
-		case '+':
+		case '+':										//'+'알때 문자입력을 한번더 받고, 그 문자를 stack에 push
 			e = _getch();
 			_putch(e);
 			push(e);
 			break;
-		case '-':
-			if (is_stack_empty())
-			{
-				printf("\n Queue is empty !!! \n");
-			}
-			else
-			{
-				e = pop();
-				printf("\n %c ", e);
-			}
+		case '-':										//'-'일때 pop
+			e = pop();
+			printf("\n %c \n", e);
 			break;
-		case 'S': stack_show(); break;
-		case 'Q': printf("\n"); exit(1);
+		case 'S':										//현재 stack상태를 보여줌
+			stack_show();
+			
+			break;
+		case 'Q':
+			printf("\n");
+			exit(1);
 		default: break;
 		}
 	}
+
+	return 0;
 }
 
-boolean is_stack_empty()
+void stack_show()
 {
-	if (top == NULL)
-		return true;
-	else
-		return false;
+	printf("\n");
+
+	for (stack_pointer p = top; p != NULL; p = p->link)
+	{
+		printf("%c ", p->item);
+	}
+
+	
+
+	/*stack_pointer p = top;
+	
+	while (p)
+	{
+		printf("%c ", p->item);
+		p = p->link;
+	}
+	*/
 }
 
 void push(Element e)
 {
 	stack_pointer temp = (stack_pointer)malloc(sizeof(stack));
 	temp->item = e;
-	temp->link = NULL;
-
-	if (is_stack_empty())
-	{
-		top = temp;
-	}
-	else
-	{
-		top->link = temp;
-		top = temp;
-	}
-
+	temp->link = top;
+	top = temp;
 }
 
 Element pop()
 {
-	stack_pointer temp;
 	Element item;
-
-	temp = top;
-	item = top->item;
-	top = NULL;
-	free(temp);
-
-	return item;
-}
-
-void stack_show()
-{
-	printf("\n");
-	for (stack_pointer i = head; i != NULL; i = i->link)
+	stack_pointer temp = top;
+	if (top == NULL)
 	{
-		printf(" %c ", i->item);
+		printf("\n\n Stack is empty! \n");
+		return 0;
+	}
+	else
+	{
+		item = temp->item;
+		top = temp->link;
+		free(temp);
+		return item;
 	}
 }
--- Homework/Problem 5/linked_stack.h
+++ Homework/Problem 5/linked_stack.h
@@ -2,21 +2,17 @@
 
 #define boolean unsigned char
 #define true 1
-#define false
+#define false 0
 typedef char Element;
-
-typedef struct stack* stack_pointer;
+// Global queue
+typedef struct stack *stack_pointer;
 typedef struct stack
 {
 	Element item;
 	stack_pointer link;
 } stack;
-
 stack_pointer top;
-stack_pointer head;
-
 void push(Element e);
 Element pop();
 void stack_show();
-boolean is_stack_full();
 boolean is_stack_empty();
