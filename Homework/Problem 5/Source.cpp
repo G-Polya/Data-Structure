722c67cde36dd9cf75472bd4bb06c5c269d3a523
김희수 <wlskaka4@gmail.com>
2017-04-18 01:36:29 +0900

    dsd

--- Homework/Problem 5/Source.cpp
+++ Homework/Problem 5/Source.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-	char *express = "a*[b-{c+(d-e)/(f-g)}-h]+(i-j)";
+	char *express = "a*[b-{c+(d-e]/(f-g)}-h]+(i-j)";
 	printf("%s", express);
 	if (check(express) == 1)
 		printf("\n 수식의 괄호가 맞게 사용되었습니다!\n");
@@ -19,26 +19,27 @@ int check(char *exp)
 	char symbol, open_pair;					//symbol:문자열 원소, open_pair: 짝맞추기 위해
 	int length = strlen(exp);
 	top = NULL;
-	for (int i = 0; exp[i] != NULL;i++)		//문자열길이만큼, 즉 문자열 끝까지. for(int i = 0; exp[i] != NULL; i++)로 대체가능
+	for (int i = 0; i < length; i++)		//문자열길이만큼, 즉 문자열 끝까지. for(int i = 0; exp[i] != NULL; i++)로 대체가능
 	{
-		symbol = exp[i];
+		symbol = exp[i];					//symbol은 parameter로 입력된 문자열의 원소; 반복문으로 인해 문자열의 모든원소가 다음반복이 되기전까지 symbol에 대입된다
+											//결국 문자열의 모든 원소에 대해 검사(작업)이 이루어진다.
 		switch (symbol)
 		{
 		case'(':
 		case'{':
 		case'[':
 			push(symbol);
-			break;
+			break;							//문자열의 원소가 '(', '{' '['이면 그 원소를 push
 		case')':
 		case'}':
 		case']':
-			if (top == NULL)
+			if (is_stack_empty())				//문자열의 원소가 ')', '}', ']'이고 stack이 비었으면
 				return 0;
 			else
 			{
-				open_pair = pop();
+				open_pair = pop();				//문자열의 원소가 ')', '}', ']'이지만 stack이 비어있지 않았으면, stack의 마지막 원소를 pop해서 open_pair에 저장
 				if ((open_pair == '(' && symbol != ')') || (open_pair == '{' && symbol != '}')
-					|| (open_pair == '[' && symbol != ']'))
+					|| (open_pair == '[' && symbol != ']'))						//open_pair와 symbol이 짝이 맞으면. 즉, 왼쪽 오른쪽 괄호수가 같고, 
 					return 0;
 				else
 					break;
@@ -49,6 +50,14 @@ int check(char *exp)
 		return 1;
 	else
 		return 0;
+}
+
+boolean is_stack_empty()
+{
+	if (top == NULL)
+		return true;
+	else
+		return false;
 }
 
 void stack_show()
@@ -80,7 +89,7 @@ Element pop()
 {
 	Element item;
 	stack_pointer temp = top;
-	if (top == NULL)
+	if (is_stack_empty())
 	{
 		printf("\n\n Stack is empty! \n");
 		return 0;
@@ -91,35 +100,5 @@ Element pop()
 		top = temp->link;
 		free(temp);
 		return item;
-	}
-}
-
-Element peek()
-{
-	Element item;
-	if (top == NULL)
-	{
-		printf("\n\n Stack is empty!\n");
-		return 0;
-	}
-	else
-	{
-		item = top->item;
-		return item;
-	}
-}
-
-void del()
-{
-	stack_pointer temp;
-	if (top == NULL)
-	{
-		printf("\n\n Stack is empty!\n");
-	}
-	else
-	{
-		temp = top;
-		top = top->link;
-		free(temp);
 	}
 }
